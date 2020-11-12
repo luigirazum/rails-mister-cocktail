2705f94cb5539a27c42142398c89866c241ca834
Luis Zubia <soyzubia@gmail.com>
2020-11-12 13:26:54 -0700

    Ingredients have been seeded... OK.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+require 'rest-client'
+require 'json'
+
+ingredients_list = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+general_response = RestClient.get ingredients_list
+repos_general_response = JSON.parse(general_response)
+ingredients_seed = repos_general_response["drinks"]
+
+puts "Seeding Ingredients... started..."
+
+ingredients_seed.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+  puts "Adding #{ingredient["strIngredient1"]}"
+end
+
+puts "Seeding Ingredients... finished..."
