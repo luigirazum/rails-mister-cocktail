64a5eb1f28f41433ae7ea44b08d48d7a6c28b7df
Luis Zubia <soyzubia@gmail.com>
2020-11-13 15:14:48 -0700

    Fixed routes, to respond on root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get root to: "cocktails#index"
+
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     resources :doses, only: [ :new, :create ]
   end
